b3ff093515ed4679e44d5a6903396abfd87baf78
teacherIan <iansmalloy@gmail.com>
2024-10-26 17:22:55 +0800

    updates...

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,20 +1,8 @@
 import { useGLTF, OrbitControls, Sky } from '@react-three/drei';
-import { Perf } from 'r3f-perf';
-import {
-  InstancedRigidBodies,
-  CylinderCollider,
-  RigidBody,
-  Physics,
-} from '@react-three/rapier';
-
+import { Physics } from '@react-three/rapier';
 import Lights from './Lights';
 import Floor from './Floor';
 import FullScene from './FullScene';
-import SquareLeftLoader from './SquareLeftLoader';
-import SquareRightLoader from './SquareRightLoader';
-import SquareMiddleLoader from './SquareMiddleLoader';
-import ILoader from './ILoader';
-import ALoader from './ALoader';
 import Cissp from './Cissp';
 import CCNA from './CCNA';
 import Aws from './Aws';
@@ -31,11 +19,7 @@ export default function Experience() {
         <Lights />
         <Floor />
         <MainLoader />
-        {/* <ALoader xOffset={6} /> */}
-        {/* <ILoader xOffset={40} /> */}
-        {/* <SquareRightLoader xOffset={-44} /> */}
-        {/* <SquareMiddleLoader xOffset={-29} /> */}
-        {/* <SquareLeftLoader xOffset={-20} /> */}
+
         {/* <Cissp /> */}
         {/* <CCNA /> */}
         {/* <Aws /> */}
--- src/MainLoader.jsx
+++ src/MainLoader.jsx
@@ -33,7 +33,7 @@ export default function MainLoader({ xOffset }) {
       >
         <instancedMesh castShadow receiveShadow args={[null, null, cubesCount]}>
           <sphereGeometry args={[1.5]} />
-          <meshStandardMaterial color="#7C9EB2" />
+          <meshBasicMaterial color="#7C9EB2" />
         </instancedMesh>
       </InstancedRigidBodies>
     </>
