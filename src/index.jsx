a52766e6f0751bbe2c9c586c7b069c58472f67c7
teacherIan <iansmalloy@gmail.com>
2024-10-26 17:16:05 +0800

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ root.render(
     camera={{
       fov: window.innerWidth < 1400 ? 70 : 40,
       near: 1,
-      far: 2000,
+      far: 500,
       position: [
         window.innerWidth < 1400 ? -50 : 50,
         window.innerWidth < 1400 ? 30 : 30,
