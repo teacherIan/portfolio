e1e35d337b27070f8da2d4c4562c7a0e34f02792
teacherIan <iansmalloy@gmail.com>
2024-10-30 12:04:23 +0800

    updates...

--- src/Dfw.jsx
+++ src/Dfw.jsx
@@ -2,7 +2,7 @@ import { useGLTF } from '@react-three/drei';
 import { RigidBody } from '@react-three/rapier';
 import tableObj from '../src/assets/table_new.glb';
 import { Suspense, useState } from 'react';
-import { Outlines, useCursor } from '@react-three/drei';
+import { Outlines, useCursor, Text } from '@react-three/drei';
 
 export default function Dfw() {
   const [hovered, set] = useState(false);
@@ -13,28 +13,30 @@ export default function Dfw() {
 
   const { nodes, materials } = useGLTF(tableObj);
   return (
-    <RigidBody
-      restitution={0.5}
-      rotation={[0, Math.PI * -1.1, 0]}
-      type="dynamic"
-      scale={25}
-      position={[62, 10, -20]}
-      colliders="cuboid"
-    >
-      <group dispose={null}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.mesh_0.geometry}
-          material={nodes.mesh_0.material}
-          onPointerOver={() => set(true)}
-          onPointerOut={() => set(false)}
-          onClick={handleClick}
-        >
-          <Outlines thickness={hovered ? 5 : 0} color="white" />
-        </mesh>
-      </group>
-      {/* <primitive object={table.scene} scale={2} /> */}
-    </RigidBody>
+    <Suspense fallback={<Text>Hello</Text>}>
+      <RigidBody
+        restitution={0.5}
+        rotation={[0, Math.PI * -1.1, 0]}
+        type="dynamic"
+        scale={25}
+        position={[62, 10, -20]}
+        colliders="cuboid"
+      >
+        <group dispose={null}>
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Mesh_0.geometry}
+            material={materials['Material_0.001']}
+            onPointerOver={() => set(true)}
+            onPointerOut={() => set(false)}
+            onClick={handleClick}
+          >
+            <Outlines thickness={hovered ? 5 : 0} color="white" />
+          </mesh>
+        </group>
+        {/* <primitive object={table.scene} scale={2} /> */}
+      </RigidBody>
+    </Suspense>
   );
 }
