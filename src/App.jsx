def25ef51168d91452155665da69afc8bf8efbd3
teacherIan <iansmalloy@gmail.com>
2024-11-09 12:03:29 +0800

    updates...

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,44 @@ import Experience from './Experience.jsx';
 import { Suspense, useState, useEffect } from 'react';
 import { KeyboardControls, Grid } from '@react-three/drei';
 import { Leva } from 'leva';
+import {
+  FaLongArrowAltLeft,
+  FaLongArrowAltRight,
+  FaLongArrowAltUp,
+  FaLongArrowAltDown,
+} from 'react-icons/fa';
 
 import Loading from './Loading.jsx';
 
 export default function App() {
   return (
     <>
-      <Leva />
+      <Leva hidden />
+      <div className="container">
+        <div className="arrow">
+          <FaLongArrowAltLeft />
+        </div>
+        <div className="arrow">
+          <FaLongArrowAltRight />
+        </div>
+        <div className="arrow">
+          <FaLongArrowAltUp />
+        </div>
+        <div className="arrow">
+          <FaLongArrowAltDown />
+        </div>
+      </div>
+
       <Canvas
         camera={{
-          fov: window.innerWidth < 1400 ? 70 : 35,
+          // 82
+          fov: window.innerWidth < 1400 ? 90 : 35,
           near: 1,
           far: 2000,
           position: [
             window.innerWidth < 1400 ? 0 : 0,
-            window.innerWidth < 1400 ? 30 : 100,
-            window.innerWidth < 1400 ? -120 : -230,
+            window.innerWidth < 1400 ? 100 : 100,
+            window.innerWidth < 1400 ? -220 : -220,
           ],
         }}
       >
